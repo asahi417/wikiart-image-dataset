a0910f7dbcb594e9011b66e939bcebea0ed4b222
asahi417 <asahi1992ushio@gmail.com>
2021-12-04 21:00:04 +0000

    init

--- setup.py
+++ setup.py
@@ -36,7 +36,8 @@ setup(
         'opencv-python',
         "mediapipe",
         # "ISR",
-        'ISR @ git+ssh://git@github.com/asahi417/image-super-resolution@v2.2.0#egg=ISR',
+        # 'ISR @ git+ssh://git@github.com/asahi417/image-super-resolution@v2.2.0#egg=ISR',
+        'git+https://github.com/asahi417/image-super-resolution.git',
         "pillow",
         "gdown"
     ]
